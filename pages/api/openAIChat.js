7af623e4199aef4611a2b2337a0771d1e883a712
dinucciagus <dinucciagus@gmail.com>
2023-05-09 10:36:04 +0200

    dotenv

--- pages/api/openAIChat.js
+++ pages/api/openAIChat.js
@@ -1,10 +1,10 @@
 import { Configuration, OpenAIApi } from "openai";
-
+require("dotenv").config();
 const configuration = new Configuration({
-  //   organization: process.env.NEXT_PUBLIC_OPENAI_ORGANIZATION,
-  //   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-  organization: "org-6HJV7f0t2RwkNElcUTHLgz8r",
-  apiKey: "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",
+  organization: process.env.NEXT_PUBLIC_OPENAI_ORGANIZATION,
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  // organization: "org-6HJV7f0t2RwkNElcUTHLgz8r",
+  // apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 export default async function handler(req, res) {
@@ -13,5 +13,5 @@ export default async function handler(req, res) {
     messages: req.body.messages,
     // messages: [{ role: "user", content: "hello" }],
   });
-  res.status(200).json({ result: completion.data.choices[0].message });
+  res.status(200).json({ result: completion.data });
 }
